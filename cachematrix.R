15747261260f6821fce13a4b55e2f5cdf6c4f7fd
Joel Louzado <jlouzado@gmail.com>
2014-09-20 20:56:16 +0530

    Defined makeCacheMatrix
    Similar template to the makeVector thing in the example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Constructor function for special Matrix which has associated `set`
+## and `get` functions for both the matrix and it's inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    # Define setter function for matrix
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    #define getter function for matrix
+    get <- function() x
+    # Define setter function for inverse
+    setInv <- function(inverse) inv <<- inverse
+    # Define getter function for matrix
+    getInv <- function() inv
+    matrix(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Function that returns matrix inverse from cache if available, 
+## and calculates (and stores it) if it's not yet available.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
 }
